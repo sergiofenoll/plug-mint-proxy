02764a1cdb9625f27e7484e1f25e5cb209f97046
Aad Versteden <madnificent@gmail.com>
2019-09-20 14:45:48 +0200

    Comment out launching of cowboy in PlugMintProxy supervisor
    Launching the supervisor should probably be handled by a consuming
    application rather than by this application.  As such we'll be relying
    on others to provide this functionality.
    
    Related code is kept in this repository as it helps understand how the
    proxy can be used.  Supporting code will be added as we go along.

--- lib/plug_mint_proxy.ex
+++ lib/plug_mint_proxy.ex
@@ -9,17 +9,17 @@ defmodule PlugMintProxy do
   require Logger
 
   def start(_type, _args) do
-    public_port_env =
-      System.get_env("PROXY_PORT") && elem(Integer.parse(System.get_env("PROXY_PORT")), 0)
+    # public_port_env =
+    #   System.get_env("PROXY_PORT") && elem(Integer.parse(System.get_env("PROXY_PORT")), 0)
 
-    port = public_port_env || 8888
+    # port = public_port_env || 8888
 
     children = [
       {ConnectionPool, {}},
-      {Plug.Cowboy, scheme: :http, plug: Proxy, options: [port: port]}
+      # {Plug.Cowboy, scheme: :http, plug: Proxy, options: [port: port]}
     ]
 
-    Logger.info("PlugMintProxy started on #{port}")
+    # Logger.info("PlugMintProxy started on #{port}")
 
     Supervisor.start_link(children, strategy: :one_for_one)
   end
